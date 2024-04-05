e1d37ae14899215d246b0da07bbf03dea9100ee9
Vitaliy Dechyshyn <demchyshyn.v87@gmail.com>
2024-04-05 19:08:18 +0200

    fix main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {UsersComponent} from "./components/users/users.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UsersComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
